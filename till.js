89da559e79288afb98d6aae082cbae645a98a5f1
iseeteaj# <iseetea@gmail.com>
2017-06-26 19:07:24 +0100

    different fruits

--- till.js
+++ till.js
@@ -3,4 +3,18 @@ var apples = {
     price:0.99
 }
 
+var oranges = {
+    name:"oranges",
+    price:0.60
+}
+
+var bananas = {
+    name:"bananas",
+    price:0.16
+}
+
+var strawberries = {
+    name:"strawberries",
+    price:2.00
+}
 var products = [apples, oranges, bananas, strawberries]  
